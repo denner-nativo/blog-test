de5af4b24f29a3d37ab98a3392dfc540d8b6c42d
Denner Portuguez <denner.portuguez@nativo.la>
2022-05-12 12:15:12 -0600

    ADD: Users migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -15,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'lastname', 'email', 'password', 'role_id', 'last_login'
     ];
 
     /**
@@ -27,4 +28,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'last_login' => 'datetime',
+    ];
+
+    public function userType()
+    {
+        return $this->hasOne('App\Models\UserType');
+    }
 }
