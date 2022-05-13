99c5483d8a2a2534d2b8af3c7b57ec96eb0e1fd3
Denner Portuguez <denner.portuguez@nativo.la>
2022-05-13 09:31:24 -0600

    Modify blog migration
    Edit name -> title

--- database/migrations/2022_05_13_030025_create_blogs_table.php
+++ database/migrations/2022_05_13_030025_create_blogs_table.php
@@ -15,7 +15,7 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('title');
             $table->string('description');
             $table->unsignedInteger('user_id');
             $table->boolean('active');
